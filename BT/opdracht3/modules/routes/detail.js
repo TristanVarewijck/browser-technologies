13691ef406df738a998de23590eb31c50153fc6b
TristanVarewijck <tristanconnect4@outlook.com>
2022-04-01 10:20:33 +0200

    detail page

--- BT/opdracht3/modules/routes/detail.js
+++ BT/opdracht3/modules/routes/detail.js
@@ -3,6 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/photo/:id", function (req, res, next) {
+  let { id } = req.params; // ID by image
+  let items = [];
+
+  for (var i = 0; i < localStorage.length; i++) {
+    items.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+  }
+  let detailItem = items.find((i) => i.image === id);
+  console.log(detailItem);
   res.render("detail");
 });
 
