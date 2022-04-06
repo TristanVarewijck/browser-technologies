1f4a59c579f97200976f25dec5dd786555c643e5
TristanVarewijck <tristanconnect4@outlook.com>
2022-04-06 13:56:08 +0200

    layout switch

--- BT/opdracht3/public/scripts/app.js
+++ BT/opdracht3/public/scripts/app.js
@@ -1,3 +1,4 @@
+// img preview
 function showPreview(event) {
   if (event.target.files.length > 0) {
     let src = URL.createObjectURL(event.target.files[0]);
@@ -6,3 +7,18 @@ function showPreview(event) {
     preview.style.display = "block";
   }
 }
+
+// layout changer
+const buttons = document.querySelectorAll(".layout-changer div button");
+const section = document.querySelector("main:first-of-type section");
+const a = document.querySelectorAll("main:first-of-type section a");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    section.classList = button.value;
+
+    a.forEach((a) => {
+      a.classList = button.value;
+    });
+  });
+});
